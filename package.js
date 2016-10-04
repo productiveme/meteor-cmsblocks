4c2d15b2edd29861e4b3eb798b314d24070869e6
productiveme <jaco@productive.me>
2016-10-04 10:26:57 +0200

    Code styling and default export

--- package.js
+++ package.js
@@ -1,34 +1,32 @@
 Package.describe({
-   name: 'productiveme:cmsblocks',
-   version: '0.1.9',
-   // Brief, one-line summary of the package.
-   summary: 'A content management solution for "mostly" static websites',
-   // URL to the Git repository containing the source code for this package.
-   git: 'https://github.com/productiveme/meteor-cmsblocks',
-   // By default, Meteor will default to using README.md for documentation.
-   // To avoid submitting documentation, set this field to null.
-   documentation: null
+  name: 'productiveme:cmsblocks',
+  version: '0.1.10',
+  // Brief, one-line summary of the package.
+  summary: 'A content management solution for "mostly" static websites',
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/productiveme/meteor-cmsblocks',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: null,
 });
 
-Package.onUse(function(api) {
-   api.versionsFrom('1.2.1');
-   api.use(['ecmascript','check','fourseven:scss@3.4.1','perak:markdown@1.0.5']);
-   api.use(['templating','jquery','reactive-var', 'underscore', 'tracker'], 'client');
-   api.addFiles(['lib/cmsblocks.html', 'lib/cmsblocks.scss', 'lib/cmsblocks.js', 'lib/cmsblocks-client.js'], 'client');
-   api.export('CMSBlocks');
-   if(api.mainModule) {
-      api.mainModule('lib/cmsblocks.js');
-   }
-   api.addAssets([
-      'fonts/icomoon.eot',
-      'fonts/icomoon.svg',
-      'fonts/icomoon.ttf',
-      'fonts/icomoon.woff',
-   ], 'client');
- });
+Package.onUse((api) => {
+  api.versionsFrom('1.2.1');
+  api.use(['ecmascript', 'check', 'fourseven:scss@3.4.1', 'perak:markdown@1.0.5']);
+  api.use(['templating', 'jquery', 'reactive-var', 'underscore', 'tracker'], 'client');
+  api.mainModule('lib/cmsblocks.js', ['client', 'server']);
+  api.addFiles(['lib/cmsblocks.html', 'lib/cmsblocks.scss', 'lib/cmsblocks-client.js'], 'client');
+  api.export('CMSBlocks');
+  api.addAssets([
+    'fonts/icomoon.eot',
+    'fonts/icomoon.svg',
+    'fonts/icomoon.ttf',
+    'fonts/icomoon.woff',
+  ], 'client');
+});
 
-Package.onTest(function(api) {
-   api.use(['ecmascript','tinytest']);
-   api.use('productiveme:cmsblocks');
-   api.addFiles('tests/cmsblocks-tests.js');
+Package.onTest((api) => {
+  api.use(['ecmascript', 'tinytest']);
+  api.use('productiveme:cmsblocks');
+  api.addFiles('tests/cmsblocks-tests.js');
 });
